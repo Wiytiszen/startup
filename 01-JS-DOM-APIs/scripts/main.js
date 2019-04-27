448d3caf88991861f04dc722d3a960b8a664ef36
Martin Wiytiszen <martinwiytiszen@hotmail.com>
2019-04-27 20:00:12 -0300

    I finished exercise 2

--- 01-JS-DOM-APIs/scripts/main.js
+++ 01-JS-DOM-APIs/scripts/main.js
@@ -1,3 +1,9 @@
 function showing(){
   document.querySelector(".hidden").className="show";
 }
+function showAlert(){
+  alert("hey!, you just clicked a button");
+}
+
+const button = document.querySelector("button");
+button.addEventListener("click",showAlert);
