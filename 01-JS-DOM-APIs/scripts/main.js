464209d737daf9615c9f4fae8998af2ee2b0ae83
Martin Wiytiszen <martinwiytiszen@hotmail.com>
2019-04-27 21:52:40 -0300

    exercise 3 finished

--- 01-JS-DOM-APIs/scripts/main.js
+++ 01-JS-DOM-APIs/scripts/main.js
@@ -1,9 +1,11 @@
-function showing(){
-  document.querySelector(".hidden").className="show";
-}
-function showAlert(){
-  alert("hey!, you just clicked a button");
-}
-
 const button = document.querySelector("button");
-button.addEventListener("click",showAlert);
+button.addEventListener("click",getJoke);
+
+function getJoke(){
+  const paragraph = document.querySelector('.joke-section');
+  fetch('http://api.icndb.com/jokes/random.')
+    .then(data => data.json())
+    .then(data =>{
+      paragraph.innerHTML = `${data.value.joke}`;
+    });
+}
