2131dd8b85c7bb647335f774ac6bfc2401db5445
Martin Wiytiszen <martinwiytiszen@hotmail.com>
2019-05-01 19:56:19 -0300

    exercise 4 finished

--- 01-JS-DOM-APIs/scripts/main.js
+++ 01-JS-DOM-APIs/scripts/main.js
@@ -1,9 +1,30 @@
-function doSearch(params ="javascript"){
+
+/**
+ * exercise 4 Topic 01
+ */
+
+const btn = document.querySelector('button');// select button
+
+btn.addEventListener("click",()=>{ // create an click event
+  let input = document.querySelector('#search').value; 
+  let ul = document.querySelector('#listResult');
+  // if <ul> has another search inside, this will be deleted; 
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild);
+  }
+  if(input){ // check if it has a value
+    doSearch(input);
+  }
+});
+
+
+function doSearch( params='Javascript'){ // default parameter "Javascript"
   let url = "https://api.github.com/search/repositories?q="+params;
     fetch(url)
     .then((data)=>data.json())
     .then((json)=>{
       json.items.forEach((element) => {
+        // create elements
         let ul = document.querySelector('#listResult');
         let li = document.createElement('li');
         let anchor = document.createElement('a');
@@ -16,5 +37,3 @@ function doSearch(params ="javascript"){
     })
     .catch((error)=> console.log("something went wrong"));
 }
-
-doSearch();
