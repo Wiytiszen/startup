5cb6a755f416e1c64a4d423251ad6679da3b053f
Martin Wiytiszen <martinwiytiszen@hotmail.com>
2019-05-01 15:17:06 -0300

    Make a CORS request

--- 01-JS-DOM-APIs/scripts/main.js
+++ 01-JS-DOM-APIs/scripts/main.js
@@ -1,40 +1,24 @@
-// create an object
-const config = {
-  url: "http://api.icndb.com/jokes/random",
-  method: "GET"
-}
-// select button
-const button = document.querySelector("button");
-  button.addEventListener("click",()=>
-  getInfo(config)
-  .then(data => JSON.parse(data))
-  .then((json)=> {
-    const paragraph = document.querySelector('.joke-section');
-      paragraph.innerHTML = json.value.joke;
-    })
-  .catch((error)=> {
-    const section = document.querySelector('.mainSection');
-    section.style.backgroundColor="red";
-    const paragraph = document.querySelector('.joke-section');
-    paragraph.innerHTML = "Ups , something went wrong";
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+    xhr.open(method, url, true);
+    return xhr;
+  
+  } 
+function MakeCORSRequest(params ="javascript"){
+  let url = `https://api.github.com/search/repositories?q=${params}`;
+  const xhr = createCORSRequest('GET',url);
+  if (!xhr) {
+    throw new Error('CORS not supported');
   }
-));
-// reusable function
-// it takes an object as parameter
-// returns a promise
-function getInfo(obj){
-    return new Promise((response,reject)=>{
-      var xhttp = new XMLHttpRequest();
-      xhttp.open(obj.method,obj.url,true);
-      xhttp.send();
-      xhttp.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status ==200){
-          response(this.response);
-        }else if(this.status == 404){
-          reject("Error");
-        }
-    };
-  });
+  xhr.onload = function() {
+    var responseText = xhr.responseText;
+    console.log(responseText);
+    // process the response.
+   };
+   
+   xhr.onerror = function() {
+     console.log('There was an error!');
+   };
+   xhr.send();
 }
-
-
+MakeCORSRequest();
