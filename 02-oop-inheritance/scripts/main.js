420b854e01b0a970690de18746ebdb9fac4f6e79
Martin Wiytiszen <martinwiytiszen@hotmail.com>
2019-05-07 19:10:37 -0300

    Exercise 1 -Create EventEmitter

--- 02-oop-inheritance/scripts/main.js
+++ 02-oop-inheritance/scripts/main.js
@@ -1,28 +1,45 @@
-class Movie{
-  constructor(name,year,duration){
-    this.title = name;
-    this.year = year;
-    this.duration =duration;
-  }
-  play(){
-    console.log(`Playing ${this.title}`);
-  }
-  pause(){console.log("Paused")}
-  resume(){console.log("Keep playing")}
-}
 class Actor{
   constructor(name,age){
     this.name = name;
     this.age = age;
   }
 }
+
+class EventEmitter{
+
+  constructor(){
+    this.listeners = {}; //guardar los eventos en Arrays, con sus respectivos Callbacks
+  }
+
+  on(eventName,callBacks){ // cargo los eventos nuevos o los callbacks
+    if(!this.listeners[eventName]){// si el evento existe en "events"... 
+      this.listeners[eventName]=[];// cargo en el evento existente una funcion
+    }
+    this.listeners[eventName].push(callBacks);
+  }
+  emit(eventName){ // Avisa que sucede un evento
+    if (this.events[eventName]){
+      this.events[eventName].forEach( fn => {
+         fn.apply(); 
+        }
+      );
+    }
+  }
+  off(eventName,callBacks){
+    let lis = this.events[eventName];
+    if (!lis){ return this; }
+    for(let i = lis.length; i > 0; i--) {
+      if (lis[i] === callBacks) {
+        lis.splice(i,1);
+        break;
+      }
+    }
+  }
+}
+
 // instances of Movie Class
 const movie1 = new Movie("Scent of a woman",1992,156);
 const movie2 = new Movie("Titanic",1997,195);
 const movie3 = new Movie("Die unendliche Geschichte",1984,102);
 const movie4 = new Movie("King's Lion",1994,47);
 const movie5 = new Movie("Gladiator",1992,155);
-
-// instances of Actor
-
-const actor1 = new Actor("Saul",46);
