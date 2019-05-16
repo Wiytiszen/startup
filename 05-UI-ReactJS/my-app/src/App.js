98a280f0aaf84fb6bdc325a86351d5d6e25f98df
Martin Wiytiszen <martinwiytiszen@hotmail.com>
2019-05-16 00:12:48 -0300

    Create Movies

--- 05-UI-ReactJS/my-app/src/App.js
+++ 05-UI-ReactJS/my-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MovieList from './components/ListOfMovies';
+import CreateMovie from './components/CreateMovie';
 
 export default class App extends React.Component{
   constructor(props){
@@ -9,10 +10,21 @@ export default class App extends React.Component{
         {id:0,title:"Up",duration :"80min"}
       ],
     }
+    this.createMovie = this.createMovie.bind(this);
+  }
+  createMovie(movie){
+
+    const {title,duration} = movie;
+    if(title && duration){
+      this.setState({
+        movies: [ ...this.state.movies,{id:0,title,duration}]
+      }) ;
+    }
   }
   render(){
     return(
     <div>
+      <CreateMovie createMovie ={this.createMovie}/>
       <MovieList 
         movies={this.state.movies}
       />
